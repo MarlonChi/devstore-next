bc226348e4d7fd85b59fbef9b09bede07858ed77
MarlonChi <marlonchiodelli@hotmail.com>
2024-04-22 19:53:36 -0300

    feat: suspense

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -11,7 +12,9 @@ export function Header() {
           devstore
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
